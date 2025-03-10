72a579eaa756fdbe250105a6210664a4687ff7d5
David <davidsaaka64@gmail.com>
2025-03-09 22:22:41 -0500

    Updated and added 2 methods for sorting and Alumni of 30years

--- src/main/java/edu/miu/cs/cs425/studentrecordsmgmtapp/model/Student.java
+++ src/main/java/edu/miu/cs/cs425/studentrecordsmgmtapp/model/Student.java
@@ -4,11 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Student {
-    private int studentId;
+    private long studentId;
     private String name;
-    private String dateOfAdmission;
+    private LocalDate dateOfAdmission;
+
+    public void setDateOfAdmission(String dateOfAdmission) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        this.dateOfAdmission = LocalDate.parse(dateOfAdmission, formatter);
+    }
 }
